4e74f0a4c5f0867ed4c2f496bed3ef898d502350
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-09 23:33:39 +0700

    edit FIS and DT

--- FISandDecissionTree/entropy.py
+++ FISandDecissionTree/entropy.py
@@ -1,8 +1,19 @@
+from glob import glob
 import math
 
 
-def calculate(a, b):
+def entropy(a, b):
     return -a * math.log(a, 2) - b * math.log(b, 2)
 
 
-print(calculate((9/14), (5/14)).3f)
+def multiple(a, b):
+    return a * b
+
+
+globalEntropy = entropy(2/5, 3/5)
+# print("%.3f" % globalEntropy)
+
+
+print("%.3f" % entropy((1/2), (1/2)))
+
+print("%.3f" % (0.971 - (multiple((3/5), 0.918) + multiple((2/5), 1))))
